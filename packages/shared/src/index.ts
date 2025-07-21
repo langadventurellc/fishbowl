c7ae9725c61615ca876dfb8ef3078f93b47e51d3
Zach Shewmaker <zach@langadventure.com>
2025-07-21 18:06:05 -0500

    fix tsconfigs

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,8 +1,1 @@
 // Core exports for Fishbowl shared package
-export * from './types';
-export * from './hooks';
-export * from './store';
-export * from './utils';
-export * from './services';
-export * from './api';
-export * from './db';
--- packages/ui-theme/src/index.ts
+++ packages/ui-theme/src/index.ts
@@ -1,4 +1,1 @@
 // Design tokens and theme for Fishbowl apps
-export * from './colors';
-export * from './spacing';
-export * from './typography';
